4adc3bfcb76321f4892afc86c68be102271c8872
evoup <evoex123@gmail.com>
2013-09-11 16:26:36 +0800

    c

--- Classes/EnemyClass.h
+++ Classes/EnemyClass.h
@@ -2,5 +2,16 @@
 // enemy class 
 //
 @interface EnemyClass : NSObject {
+    CCSprite *_spriteUnit;
+    CCAction *_spriteWalkAction;
+}
+@property (nonatomic, retain) CCSprite *spriteUnit;
+@property (nonatomic, retain) CCAction *spriteWalkAction;
+
+
+-(void)initWithSynthesize (CCSprite *)_spu (CCAction *)_spwa
+{
+    @synthesize spriteUnit = _spu; 
+    @synthesize spriteUnit = _spwa; 
 }
 @end
