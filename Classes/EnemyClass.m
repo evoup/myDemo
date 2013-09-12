201072d08930f017bb68cdc4e6fb8c7a9aff28a6
evoup <evoex123@gmail.com>
2013-09-12 17:12:08 +0800

    c

--- Classes/EnemyClass.m
+++ Classes/EnemyClass.m
@@ -37,6 +37,19 @@
      @"AnimPlayerUnit01.plist"];
     CCSpriteBatchNode *spriteSheet = [CCSpriteBatchNode batchNodeWithFile:@"AnimPlayerUnit01.png"];
     [cid addChild:spriteSheet z:zid];
+    NSMutableArray *walkAnimFrames = [NSMutableArray array];
+    for(int j = 0; j <= 1; ++j) {
+        [walkAnimFrames addObject:[[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName:[NSString stringWithFormat:@"playerUnit01_0%d.png", j]]];
+    }
+    CCAnimation *walkAnim = [CCAnimation animationWithFrames:walkAnimFrames delay:0.1f];
+
+    //self.spriteUnit = [CCSprite spriteWithSpriteFrameName:@"playerUnit01_00.png"];
+    //self.spriteWalkAction = [CCRepeatForever actionWithAction:
+                       //[CCAnimate actionWithAnimation:walkAnim restoreOriginalFrame:NO]];
+    //CGSize winSize = [[CCDirector sharedDirector] winSize];
+    //_spriteUnit.position = ccp(winSize.width/2+20, winSize.height/2);
+    //[_spriteUnit runAction:_spriteWalkAction];
+    //[_spriteUnit addChild:_spriteUnit];
 }
 
 @end
